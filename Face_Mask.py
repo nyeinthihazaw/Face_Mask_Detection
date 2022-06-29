4542d383cbea304fb1d3a03a52c90f2a2ee2c0e0
Nyein Thiha Zaw <104121152+nyeinthihazaw@users.noreply.github.com>
2022-06-29 10:00:56 +0630

    Update Face_Mask.py

--- Face_Mask.py
+++ Face_Mask.py
@@ -15,16 +15,16 @@ if uploaded_file is not None:
 	
 	image = cv2.imread(uploaded_file)
 	st.image(uploaded_file, caption='Input Image', use_column_width=True)
-   	img=cv2.resize(image,(240,240),interpolation=cv2.INTER_AREA)
-    	img_g=cv2.GaussianBlur(img,(3,3),0,0)
-    	img_m=cv2.medianBlur(img,3)
-    	img_c=cv2.Canny(img_g,50,150)
-    	fd=img_c.flatten()
-    	fd=fd.reshape(1,-1)
-    	result = model.predict(fd)
-    	if result == [1]:
-        	st.write('With Mask')
-    	elif result == [0]:
-        	st.write('Without Mask');
-    	else:
-        st.write('No Face Here')
+	img=cv2.resize(image,(240,240),interpolation=cv2.INTER_AREA)
+	img_g=cv2.GaussianBlur(img,(3,3),0,0)
+	img_m=cv2.medianBlur(img,3)
+	img_c=cv2.Canny(img_g,50,150)
+	fd=img_c.flatten()
+	fd=fd.reshape(1,-1)
+	result = model.predict(fd)
+	if result == [1]:
+		st.write('With Mask')
+	elif result == [0]:
+		st.write('Without Mask');
+	else:
+		st.write('No Face Here')

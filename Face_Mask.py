c7097a4d82412a6c4bd8fa87c6b742826e291015
Nyein Thiha Zaw <104121152+nyeinthihazaw@users.noreply.github.com>
2022-06-29 16:54:10 +0630

    Update Face_Mask.py

--- Face_Mask.py
+++ Face_Mask.py
@@ -9,11 +9,11 @@ model= joblib.load('models.dat')
 st.header("Face Mask Detection")
 st.write("Choose any image :")
 uploaded_file = st.file_uploader("Choose an image...")
-min = st.slider('Minimun Intensity for Canny Edge', 0, 255, 50)
-max = st.slider('Maximun Intensity for Canny Edge', 0, 255, 50)
-kernel_size = st.selectbox(
-     'Kernel size for Gaussian Blur',
-     ((3,3), (5,5))
+# min = st.slider('Minimun Intensity for Canny Edge', 0, 255, 50)
+# max = st.slider('Maximun Intensity for Canny Edge', 0, 255, 50)
+# kernel_size = st.selectbox(
+#      'Kernel size for Gaussian Blur',
+#      ((3,3), (5,5))
 if uploaded_file is not None:
 	
 	image = Image.open(uploaded_file)
@@ -22,9 +22,9 @@ if uploaded_file is not None:
 	cv2.imwrite('out.png', cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR))
 	image =cv2.imread('out.png')
 	img=cv2.resize(image,(240,240),interpolation=cv2.INTER_AREA)
-	img_g=cv2.GaussianBlur(img,kernel_size,0,0)
+	img_g=cv2.GaussianBlur((3,3),0,0)
 # 	img_m=cv2.medianBlur(img,3)
-	img_c=cv2.Canny(img_g,min,max)
+	img_c=cv2.Canny(img_g,50,150)
 # 	st.image(img_g, caption='Gaussian Blur', use_column_width=True)
 	st.image(img_c, caption='Canny Edge Detection', width=300)
 	fd=img_c.flatten()
